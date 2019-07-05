0665b7c6a65884305bffa656916ac7bf41390119
Arne Tempelhof <arnetempelhof@gmail.com>
2019-07-05 13:56:06 +0200

    Removed dependency

--- Package.swift
+++ Package.swift
@@ -4,8 +4,11 @@ import PackageDescription
 
 let package = Package(
     name: "ResearchKit",
-    dependencies: [
-        .package(url: "https://github.com/webventil/ResearchKit.git", from: "2.0.0"),
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "ResearchKit",
+            targets: ["ResearchKit"]),
     ],
     targets: [
         .target(
